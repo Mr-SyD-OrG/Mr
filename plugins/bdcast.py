1fd98f98cfcee2e3e0c1837e5dd38a67e78a88db
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-03-31 13:09:07 +0530

    Update bdcast.py

--- plugins/bdcast.py
+++ plugins/bdcast.py
@@ -26,8 +26,8 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("cinemagoer").setLevel(logging.ERROR)
 logger = logging.getLogger(__name__)
 
-DATABASE_NAME =
-DATABASE_URL = 
+DATABASE_NAME = Cluster0
+DATABASE_URL = mongodb+srv://MrMov:MrMov@cluster0.jazglqb.mongodb.net/
 BOT_TOKEN = 6006903252:AAGtqD6NIFHvZkqK1Pmhgfs8hkDXCg7u_iM
 LOG_CHANNEL = -1002139569083
 
