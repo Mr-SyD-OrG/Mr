9c2a4aac4255df5454dedd1b922f8b30500a881b
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-10 18:06:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import logging, os, re, asyncio, requests, aiohttp 
+import logging, os, re, asyncio, requests, aiohttp, re, http.client
 from pyrogram.errors import *                           
 from pyrogram.types import Message, InlineKeyboardButton
 from pyrogram import filters, enums
@@ -34,7 +34,21 @@ class temp(object):
     PM_BUTTONS = {}
     PM_SPELL = {}
     GP_SPELL = {}
-
+    
+async def pub_is_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append(
+                [InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)]
+            )
+        except Exception as e:
+            pass
+    return btn
+    
 async def is_subscribed(bot, query):
     if REQUEST_TO_JOIN_MODE == True and join_db().isActive():
         try:
