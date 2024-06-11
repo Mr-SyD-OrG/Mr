4ffaa4356e69fa905732284c73e1e5bda8f52eb2
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-11 06:58:45 +0530

    Update syd.py

--- plugins/syd.py
+++ plugins/syd.py
@@ -13,22 +13,22 @@ async def del_requests(client, message):
     await db.del_join_req()    
     await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
 
-@Client.on_chat_join_request(filters.chat(SYD_CHANNEL))
-async def join_reqs(client, message: ChatJoinRequest):
-  if not await bd.find_join_req(message.from_user.id):
-    await bd.add_join_req(message.from_user.id)
+#@Client.on_chat_join_request(filters.chat(SYD_CHANNEL))
+#async def join_reqs(client, message: ChatJoinRequest):
+  #if not await bd.find_join_req(message.from_user.id):
+ #   await bd.add_join_req(message.from_user.id)
 
-@Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
-async def del_requests(client, message):
-    await bd.del_join_req()    
-    await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
+#@Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
+#async def del_requests(client, message):
+   # await bd.del_join_req()    
+   # await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
 
-@Client.on_chat_join_request(filters.chat(SUD_CHANNEL))
-async def join_reqs(client, message: ChatJoinRequest):
-  if not await dd.find_join_req(message.from_user.id):
-    await dd.add_join_req(message.from_user.id)
+#@Client.on_chat_join_request(filters.chat(SUD_CHANNEL))
+#async def join_reqs(client, message: ChatJoinRequest):
+ # if not await dd.find_join_req(message.from_user.id):
+   #await dd.add_join_req(message.from_user.id)
 
-@Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
-async def del_requests(client, message):
-    await dd.del_join_req()    
-    await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
+#@Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
+#async def del_requests(client, message):
+    #await dd.del_join_req()    
+    ₹await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
