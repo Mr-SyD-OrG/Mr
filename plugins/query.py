93b98a9ab04c32fb5fb04857da2dca08a5ce710b
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-11 06:54:44 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -194,7 +194,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             try: f_caption = CUSTOM_FILE_CAPTION.format(mention=query.from_user.mention, file_name='' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)                               
             except Exception as e: logger.exception(e)
         try:
-            if AUTH_CHANNEL and not await is_subscribed(client, query):
+            if AUTH_CHANNEL and not await is_sydsubscribed(client, query):
                 return await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
             elif settings['botpm']:
                 return await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
